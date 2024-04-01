a52c60d54897932e034c6cc7116178a04d883b17
Albert Arvidsson <albert.arvidsson@gmail.com>
2024-04-01 09:20:48 +0200

    Adjust nav input and select styling

--- src/App.tsx
+++ src/App.tsx
@@ -58,94 +58,96 @@ function App() {
   return (
     <Flowbite theme={{ mode: "dark" }}>
       <nav className="bg-white dark:bg-gray-900 lg:fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-        <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-1">
-          <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse"></div>
-          <div
-            className="navbar-grid items-center w-full flex-1"
-            id="navbar-sticky"
-          >
-            <a
-              href=""
-              className="flex items-center space-x-3 rtl:space-x-reverse mr-auto"
+        <div className="max-w-[2400px] mx-auto">
+          <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-1">
+            <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse"></div>
+            <div
+              className="navbar-grid items-center w-full flex-1"
+              id="navbar-sticky"
             >
-              <h1 className="self-center font-semibold dark:text-white">
-                <span className="whitespace-nowrap">🎮 Spelsyltkatalog!</span>
-              </h1>
-            </a>
-            <div className="flex place-items-center">
-              {filteredGames.length} spel!
-            </div>
-            <div className="flex justify-end items-center">
-              <DarkThemeToggle />
               <a
-                href="https://github.com/scmx/kodsnack-spelsylt-all-games-catalog"
-                className="ml-3"
+                href=""
+                className="flex items-center space-x-3 rtl:space-x-reverse mr-auto"
               >
-                <svg
-                  version="1.1"
-                  viewBox="0 0 16 16"
-                  height="32"
-                  width="32"
-                  aria-hidden="true"
-                >
-                  <path
-                    d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"
-                    fill="currentColor"
-                  ></path>
-                </svg>
+                <h1 className="self-center font-semibold dark:text-white">
+                  <span className="whitespace-nowrap">🎮 Spelsyltkatalog!</span>
+                </h1>
               </a>
+              <div className="flex place-items-center">
+                {filteredGames.length} spel!
+              </div>
+              <div className="flex justify-end items-center">
+                <DarkThemeToggle />
+                <a
+                  href="https://github.com/scmx/kodsnack-spelsylt-all-games-catalog"
+                  className="ml-3"
+                >
+                  <svg
+                    version="1.1"
+                    viewBox="0 0 16 16"
+                    height="32"
+                    width="32"
+                    aria-hidden="true"
+                  >
+                    <path
+                      d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"
+                      fill="currentColor"
+                    ></path>
+                  </svg>
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="flex justify-between align-center flex-wrap">
-          <input
-            type="search"
-            value={searchTerm}
-            onInput={handleSearch}
-            placeholder="RegExp..."
-            className="col-span-2 m-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 ps-5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          />
-          <select
-            id="select_jams"
-            className="col-span-4 m-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            value={selectedJam}
-            onChange={(e) => setSelectedJam(e.target.value)}
-          >
-            <option value={""}>Alla spelsyltar</option>
-            {Object.values(jams).map((jam) => (
-              <option key={jam.id} value={jam.id}>
-                {jam.title}
-              </option>
-            ))}
-          </select>
-          <select
-            id="select_platforms"
-            className="col-span-2 m-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            value={selectedPlatform}
-            onChange={(e) => setSelectedPlatform(e.target.value)}
-          >
-            <option value={""}>Alla plattformar</option>
-            {platforms.map((platform) => (
-              <option key={platform} value={platform}>
-                {platform}
-              </option>
-            ))}
-          </select>
-          <select
-            id="select_authors"
-            className="col-span-2 m-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            value={selectedAuthor}
-            onChange={(e) => setSelectedAuthor(e.target.value)}
-          >
-            <option value={""}>Alla syltare</option>
-            {[...authors.values()]
-              .sort(sortBy((author) => -author.submissionsCount))
-              .map((author) => (
-                <option key={author.name} value={author.name}>
-                  {author.name}
+          <div className="flex justify-between align-center flex-wrap">
+            <input
+              type="search"
+              value={searchTerm}
+              onInput={handleSearch}
+              placeholder="RegExp..."
+              className="col-span-2 m-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 ps-5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
+            <select
+              id="select_jams"
+              className="col-span-4 m-3 bg-gray-50 mr-auto border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              value={selectedJam}
+              onChange={(e) => setSelectedJam(e.target.value)}
+            >
+              <option value={""}>Alla spelsyltar</option>
+              {Object.values(jams).map((jam) => (
+                <option key={jam.id} value={jam.id}>
+                  {jam.title}
                 </option>
               ))}
-          </select>
+            </select>
+            <select
+              id="select_platforms"
+              className="col-span-2 m-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              value={selectedPlatform}
+              onChange={(e) => setSelectedPlatform(e.target.value)}
+            >
+              <option value={""}>Alla plattformar</option>
+              {platforms.map((platform) => (
+                <option key={platform} value={platform}>
+                  {platform}
+                </option>
+              ))}
+            </select>
+            <select
+              id="select_authors"
+              className="col-span-2 m-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              value={selectedAuthor}
+              onChange={(e) => setSelectedAuthor(e.target.value)}
+            >
+              <option value={""}>Alla syltare</option>
+              {[...authors.values()]
+                .sort(sortBy((author) => -author.submissionsCount))
+                .map((author) => (
+                  <option key={author.name} value={author.name}>
+                    {author.name}
+                  </option>
+                ))}
+            </select>
+          </div>
         </div>
       </nav>
       <main className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-[2400px] gap-3 mx-auto p-3 lg:mt-[123px] xl:mt-[123px] dark:bg-black">
